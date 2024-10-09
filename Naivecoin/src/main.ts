e12c8631054bfb3c57714821832cfa263e9c619b
Samrawit Gebremaryam Bahta <samrawitgebremaryam121@gmail.com>
2024-10-09 14:24:01 +0300

    Implement Minimal working blockchain

--- Naivecoin/src/main.ts
+++ Naivecoin/src/main.ts
@@ -1,24 +1,54 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as bodyParser from 'body-parser';
+import express from 'express';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { generateNextBlock, getBlockchain } from './blockchain';
+import { connectToPeers, getSockets, initP2PServer } from './p2p';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const httpPort: number = parseInt(process.env.HTTP_PORT || '3001');
+const p2pPort: number = parseInt(process.env.P2P_PORT || '6001');
+
+const initHttpServer = (myHttpPort: number) => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.use((err: any, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            next();
+        }
+    });
+
+    app.get('/blocks', (_req, res) => {
+        res.send(getBlockchain());
+    });
+
+    app.post('/mineBlock', (req, res) => {
+        if (req.body.data == null) {
+            res.status(400).send('data parameter is missing');
+            return;
+        }
+        const newBlock = generateNextBlock(req.body.data);
+        if (newBlock === null) {
+            res.status(400).send('could not generate block');
+        } else {
+            res.send(newBlock);
+        }
+    });
+
+    app.get('/peers', (_req, res) => {
+        res.send(getSockets().map((s: any) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+    });
+
+    app.post('/addPeer', (req, res) => {
+        connectToPeers(req.body.peer);
+        res.send();
+    });
+
+    app.listen(myHttpPort, () => {
+        console.log('Listening http on port: ' + myHttpPort);
+    });
+};
+
+initHttpServer(httpPort);
+initP2PServer(p2pPort);
